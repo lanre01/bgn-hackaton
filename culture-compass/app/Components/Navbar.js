e975d2e5d63facea1142d22eaba67476ae4c96d6
Kaiya kadija anisa kaye koroma <kadikay@Kaiyas-MacBook-Air.local>
2024-10-13 12:48:49 +0100

    feat: tried to add the image

--- culture-compass/app/Components/Navbar.js
+++ culture-compass/app/Components/Navbar.js
@@ -1,4 +1,5 @@
-import { AppBar, Button, Box, Stack, Typography } from "@mui/material";
+import { AppBar, Button, Box, Stack, Typography} from "@mui/material";
+import Image from "next/image";
 
 export default function Navbar() {
 
@@ -13,7 +14,14 @@ export default function Navbar() {
         padding: "10px",
         
      }}>
-        <Typography>The thing</Typography>
+        <Box>
+            <Image
+            src="/app/Resources/CC_LOGO.png" // Use absolute path from the public folder
+            alt="Culture Compass Logo"
+            width={250} // Set the width you need
+            height={100} // Set the height to maintain aspect ratio
+          />
+          </Box>
         <Box>
             <Button color="white" >About</Button>
             <Button color="white">Quiz</Button>
